b059ef235b6e6826bac16761b89ce2e0fbff9523
Cora Marcu <cora.marcu@gmail.com>
2022-05-08 16:50:54 +0100

    pass create artist test

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const { get } = require('express/lib/response');
+const artistRouter = require('./routes/artist');
 
 const app = express();
 
 app.use(express.json());
+app.use('/artist', artistRouter);
 
 app.get('/', (req, res) => {
     res.status(200);
     res.send('Hello world!');
 })
+
+
 
 module.exports = app;
